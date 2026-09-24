ca5f6a13841717ded156d608839491ed3a9bcbae
tarunsingh21126-jpg <tarunsingh21126@gmail.com>
2026-09-25 02:17:33 +0530

    path params,post req,validation json pydantic

--- BACKEND-FAST-API/main.py
+++ BACKEND-FAST-API/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
-
+class User(BaseModel):
+    name:str
+    age:int
 # HOME PAGE 
 @app.get("/")
 def home():
@@ -18,4 +20,44 @@ def about():
 def users(user_id:int):
     return {
         "user_id": user_id
+    }
+
+@app.get("/users")
+def get_users(name: str = None):
+    return {"Name": name}
+
+@app.get("/products")
+def get_users(limit:int = 10):
+    return {"limit":limit}
+
+@app.get("/items")
+def get_users(name:str=None,price:int=0):
+    return {
+        "name":name,
+        "price":price
+    }
+
+#post request
+
+@app.post("/create-user")
+def create_user(name:str,age:int):
+    return {
+        "name":name,
+        "age":age
+    }
+
+#through json format
+@app.post("/sign-user")
+def create_user(user:dict):
+    return {
+        "message":"DATA CREATED SUCESSFULLY",
+        "data":user
+    }
+
+#with validation example of pydantic
+@app.post("/sign-up")
+def create_user(user:User):
+    return {
+        "message":"DATA CREATED SUCESSFULLY pydantic",
+        "data":user
     }
